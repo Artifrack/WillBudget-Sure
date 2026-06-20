e7cccc407c89004be10ff66c1869914128f88fe6
William Zitzmann <williamzitzmann@gmail.com>
2026-06-20 15:50:47 +0100

    Fix sidekiq_embedded: pass Sidekiq.default_configuration to Embedded.new (Sidekiq 8 API change)

--- config/initializers/sidekiq_embedded.rb
+++ config/initializers/sidekiq_embedded.rb
@@ -2,16 +2,26 @@
 # Toggle by setting EMBED_SIDEKIQ=true on the web service in Railway.
 # To switch back to a separate worker: remove EMBED_SIDEKIQ and re-enable the worker service.
 if ENV["EMBED_SIDEKIQ"] == "true"
-  require "sidekiq/embedded"
-
   Rails.application.config.after_initialize do
     next if Rails.env.test?
     next if defined?(Rails::Console)
     next if defined?(Rake) && Rake.application.top_level_tasks.any?
 
+    begin
+      require "sidekiq/embedded"
+    rescue LoadError => e
+      Rails.logger.error("[SidekiqEmbedded] Cannot load sidekiq/embedded: #{e.message}. Embedded mode disabled.")
+      next
+    end
+
+    unless defined?(Sidekiq::Embedded)
+      Rails.logger.error("[SidekiqEmbedded] Sidekiq::Embedded not defined after require. Embedded mode disabled.")
+      next
+    end
+
     Rails.logger.info("[SidekiqEmbedded] Starting embedded Sidekiq within web process")
 
-    embedded = Sidekiq::Embedded.new
+    embedded = Sidekiq::Embedded.new(Sidekiq.default_configuration)
     embedded.run
 
     at_exit do
